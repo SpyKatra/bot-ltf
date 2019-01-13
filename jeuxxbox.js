0794de7bc40b966a6815376e71e2d6c9c32fd0f5
SpyKatra <46405878+SpyKatra@users.noreply.github.com>
2019-01-12 21:42:12 -0500

    Create jeuxxbox.js

---
+++ jeuxxbox.js
@@ -0,0 +1,68 @@
+//Settings!
+const yourID = (process.env.YOURID); //Instructions on how to get this: https://redd.it/40zgse
+const setupCMD = "ltf!jeuxxbox"
+let initialMessage = `**Choisis ton ou tes jeux !**`;
+const roles = ["Rainbow 6 XBOX", "Fortnite", "PUBG XBOX"];
+const reactions = [":one:", ":two:", ":three:"];
+const botToken = (process.env.TOKEN); /*You'll have to set this yourself; read more
+                     here https://github.com/reactiflux/discord-irc/wiki/Creating-a-discord-bot-&-getting-a-token*/
+
+//Load up the bot...
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+bot.login(botToken);
+
+//If there isn't a reaction for every role, scold the user!
+if (roles.length !== reactions.length) throw "Roles list and reactions list are not the same length!";
+
+//Function to generate the role messages, based on your settings
+function generateMessages(){
+    var messages = [];
+    messages.push(initialMessage);
+    for (let role of roles) messages.push(`Si tu joues à **"${role}"** c'est ici !`); //DONT CHANGE THIS
+    return messages;
+}
+
+
+bot.on("message", message => {
+    if (message.author.id == yourID && message.content.toLowerCase() == setupCMD){
+        var toSend = generateMessages();
+        let mappedArray = [[toSend[0], false], ...toSend.slice(1).map( (message, idx) => [message, reactions[idx]])];
+        for (let mapObj of mappedArray){
+            message.channel.send(mapObj[0]).then( sent => {
+                if (mapObj[1]){
+                  sent.react(mapObj[1]);  
+                } 
+            });
+        }
+    }
+})
+
+
+bot.on('raw', event => {
+    if (event.t === 'MESSAGE_REACTION_ADD' || event.t == "MESSAGE_REACTION_REMOVE"){
+        
+        let channel = bot.channels.get(event.d.channel_id);
+        let message = channel.fetchMessage(event.d.message_id).then(msg=> {
+        let user = msg.guild.members.get(event.d.user_id);
+        
+        if (msg.author.id == bot.user.id && msg.content != initialMessage){
+       
+            var re = `\\*\\*"(.+)?(?="\\*\\*)`;
+            var role = msg.content.match(re)[1];
+        
+            if (user.id != bot.user.id){
+                var roleObj = msg.guild.roles.find(r => r.name === role);
+                var memberObj = msg.guild.members.get(user.id);
+                
+                if (event.t === "MESSAGE_REACTION_ADD"){
+                    memberObj.addRole(roleObj)
+                } else {
+                    memberObj.removeRole(roleObj);
+                }
+            }
+        }
+        })
+ 
+    }   
+});
